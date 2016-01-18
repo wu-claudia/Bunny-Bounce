135381c446b13e5ed3448f2cc62e1bb7db0d144c
wu-claudia <claudiawu@dhcp-18-189-7-163.dyn.MIT.EDU>
2016-01-18 18:23:42 -0500

    indent error

--- test.py
+++ test.py
@@ -229,7 +229,7 @@ class Tree_Tall(Obstacle):
 		self.size = TREE_SIZE
 		self.rect = pygame.Surface([self.size, 2*self.size]).get_rect()
 		self.rect.topleft = (BORDER_SIZE + WIDTH - self.size, BOTTOM - 2*self.size)
-self.image = pygame.image.load(self.name_of_image).convert_alpha()
+		self.image = pygame.image.load(self.name_of_image).convert_alpha()
 		self.image = pygame.transform.smoothscale(self.image, (self.size, 2*self.size))
 
 class Rock(Obstacle):
